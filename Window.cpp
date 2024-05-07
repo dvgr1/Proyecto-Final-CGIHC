1ece6cd2c134c7dc927578481377c272850eca55
dvgr1 <77249207+dvgr1@users.noreply.github.com>
2024-05-07 10:12:15 -0600

    Add files via upload

---
+++ Window.cpp
@@ -0,0 +1,230 @@
+#include "Window.h"
+
+Window::Window()
+{
+	width = 800;
+	height = 600;
+	animacionPalanca = false;
+	moverpalanca01 = 0;
+	moverpalanca02 = 0;
+	moverpalanca03 = 0;
+	luzPaletas = true;
+	luzTablero = true;
+	musica = false;
+	cambioCamara = false;
+	camaraLibre = false;
+	subir = true;
+
+	for (size_t i = 0; i < 1024; i++)
+	{
+		keys[i] = 0;
+	}
+}
+Window::Window(GLint windowWidth, GLint windowHeight)
+{
+	width = windowWidth;
+	height = windowHeight;
+	
+	for (size_t i = 0; i < 1024; i++)
+	{
+		keys[i] = 0;
+	}
+}
+int Window::Initialise()
+{
+	//Inicialización de GLFW
+	if (!glfwInit())
+	{
+		printf("Falló inicializar GLFW");
+		glfwTerminate();
+		return 1;
+	}
+	//Asignando variables de GLFW y propiedades de ventana
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//para solo usar el core profile de OpenGL y no tener retrocompatibilidad
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	//CREAR VENTANA
+	mainWindow = glfwCreateWindow(width, height, "Proyecto Final", NULL, NULL);
+
+	if (!mainWindow)
+	{
+		printf("Fallo en crearse la ventana con GLFW");
+		glfwTerminate();
+		return 1;
+	}
+	//Obtener tamaño de Buffer
+	glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+	//asignar el contexto
+	glfwMakeContextCurrent(mainWindow);
+
+	//MANEJAR TECLADO y MOUSE
+	createCallbacks();
+
+
+	//permitir nuevas extensiones
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK)
+	{
+		printf("Falló inicialización de GLEW");
+		glfwDestroyWindow(mainWindow);
+		glfwTerminate();
+		return 1;
+	}
+
+	glEnable(GL_DEPTH_TEST); //HABILITAR BUFFER DE PROFUNDIDAD
+							 // Asignar valores de la ventana y coordenadas
+							 
+							 //Asignar Viewport
+	glViewport(0, 0, bufferWidth, bufferHeight);
+	//Callback para detectar que se está usando la ventana
+	glfwSetWindowUserPointer(mainWindow, this);
+}
+
+void Window::createCallbacks()
+{
+	glfwSetKeyCallback(mainWindow, ManejaTeclado);
+	glfwSetCursorPosCallback(mainWindow, ManejaMouse);
+	glfwSetMouseButtonCallback(mainWindow, ManejaMouseBoton);
+}
+GLfloat Window::getXChange()
+{
+	GLfloat theChange = xChange;
+	xChange = 0.0f;
+	return theChange;
+}
+
+GLfloat Window::getYChange()
+{
+	GLfloat theChange = yChange;
+	yChange = 0.0f;
+	return theChange;
+}
+
+
+
+
+void Window::ManejaTeclado(GLFWwindow* window, int key, int code, int action, int mode)
+{
+	Window* theWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+
+	if (key == GLFW_KEY_ESCAPE )
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+
+	if (key == GLFW_KEY_Z)
+	{
+		if (action == GLFW_PRESS) {
+			theWindow->moverpalanca01 = 60.0;
+		}
+		// Si la tecla Z se libera, establece el valor de moverpalanca01 en 0.0
+		else if (action == GLFW_RELEASE) {
+			theWindow->moverpalanca01 = 0.0;
+		}
+	}
+
+	if (key == GLFW_KEY_M)
+	{
+		if (action == GLFW_PRESS) {
+			theWindow->moverpalanca02 = 60.0;
+		}
+		// Si la tecla Z se libera, establece el valor de moverpalanca01 en 0.0
+		else if (action == GLFW_RELEASE) {
+			theWindow->moverpalanca02 = 0.0;
+		}
+	}
+
+	if (key == GLFW_KEY_V)
+	{
+		if (action == GLFW_PRESS) {
+			theWindow->moverpalanca03 = 60.0;
+		}
+		// Si la tecla Z se libera, establece el valor de moverpalanca01 en 0.0
+		else if (action == GLFW_RELEASE) {
+			theWindow->moverpalanca03 = 0.0;
+		}
+	}
+	
+
+	if (key == GLFW_KEY_P && action == GLFW_PRESS)
+	{
+		theWindow->luzPaletas = !(theWindow->luzPaletas);
+	}
+
+	if (key == GLFW_KEY_T && action == GLFW_PRESS)
+	{
+		theWindow->luzTablero = !(theWindow->luzTablero);
+	}
+
+	if (key == GLFW_KEY_G && action == GLFW_PRESS)
+	{
+		theWindow->musica = !(theWindow->musica);
+	}
+
+	if (key == GLFW_KEY_H && action == GLFW_PRESS)
+	{
+		theWindow->cambioCamara = !(theWindow->cambioCamara);
+	}
+	
+	if (key == GLFW_KEY_J && action == GLFW_PRESS)
+	{
+		theWindow->camaraLibre = !(theWindow->camaraLibre);
+	}
+	
+
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+		{
+			theWindow->keys[key] = true;
+			//printf("se presiono la tecla %d'\n", key);
+		}
+		else if (action == GLFW_RELEASE)
+		{
+			theWindow->keys[key] = false;
+			//printf("se solto la tecla %d'\n", key);
+		}
+	}
+}
+
+void Window::ManejaMouse(GLFWwindow* window, double xPos, double yPos)
+{
+	Window* theWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+
+	if (theWindow->mouseFirstMoved)
+	{
+		theWindow->lastX = xPos;
+		theWindow->lastY = yPos;
+		theWindow->mouseFirstMoved = false;
+	}
+
+	theWindow->xChange = xPos - theWindow->lastX;
+	theWindow->yChange = theWindow->lastY - yPos;
+
+	theWindow->lastX = xPos;
+	theWindow->lastY = yPos;
+}
+
+void Window::ManejaMouseBoton(GLFWwindow* window, int button, int action, int mods)
+{
+	Window* theWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+	
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+	{
+		theWindow->animacionPalanca = true;
+		theWindow->animacionCanica = true;
+		//printf("se presiono la tecla ");
+	}
+}
+
+Window::~Window()
+{
+	glfwDestroyWindow(mainWindow);
+	glfwTerminate();
+
+}
---
+++ Window.h
@@ -0,0 +1,64 @@
+#pragma once
+#include<stdio.h>
+#include<glew.h>
+#include<glfw3.h>
+
+class Window
+{
+public:
+	Window();
+	Window(GLint windowWidth, GLint windowHeight);
+	int Initialise();
+	GLfloat getBufferWidth() { return bufferWidth; }
+	GLfloat getBufferHeight() { return bufferHeight; }
+	GLfloat getXChange();
+	GLfloat getYChange();
+	GLboolean getAnimacionPalanca() { return animacionPalanca; }
+	GLvoid setAnimacionPalanca(bool activacion) { animacionPalanca = activacion; }
+	GLboolean getAnimacionCanica() { return animacionCanica; }
+	GLvoid setAnimacionCanica(bool activacion) { animacionCanica = activacion; }
+	GLboolean getSubir() { return subir; }
+	GLvoid setSubir(bool subida) { subir = subida; }
+	GLint getmoverpalanca01() { return moverpalanca01; }
+	GLint getmoverpalanca02() { return moverpalanca02; }
+	GLint getmoverpalanca03() { return moverpalanca03; }
+	GLboolean getluzTablero() { return luzTablero; }
+	GLboolean getluzPaletas() { return luzPaletas; }
+	GLboolean getmusica() { return musica; }
+	GLboolean getcambioCamara() { return cambioCamara; }
+	GLboolean getcamaraLibre() { return camaraLibre; }
+	bool getShouldClose() {
+		return  glfwWindowShouldClose(mainWindow);}
+	bool* getsKeys() { return keys; }
+	void swapBuffers() { return glfwSwapBuffers(mainWindow); }
+	
+	~Window();
+private: 
+	GLFWwindow *mainWindow;
+	GLint width, height;
+	bool keys[1024];
+	GLint bufferWidth, bufferHeight;
+	void createCallbacks();
+	GLfloat lastX;
+	GLfloat lastY;
+	GLfloat xChange;
+	GLfloat yChange;
+	GLint moverpalanca01 = 0;
+	GLint moverpalanca02 = 0;
+	GLint moverpalanca03 = 0;
+	
+	GLboolean animacionPalanca;
+	GLboolean animacionCanica;
+	GLboolean subir;
+	GLboolean luzTablero;
+	GLboolean luzPaletas;
+	GLboolean musica;
+	GLboolean cambioCamara;
+	GLboolean camaraLibre;
+	bool mouseFirstMoved;
+	static void ManejaTeclado(GLFWwindow* window, int key, int code, int action, int mode);
+	static void ManejaMouse(GLFWwindow* window, double xPos, double yPos);
+	static void ManejaMouseBoton(GLFWwindow* window, int button, int action, int mods);
+
+};
+
